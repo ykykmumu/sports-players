56e3ae9482c884be4122418dfa1be788f514e1cd
西村祐輝 <nishimurayuki@mullan-mac.local>
2019-12-29 15:02:30 +0900

    カラムのイメージ追加

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
