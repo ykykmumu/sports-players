015a9d302cd82c0b21ab3f918dc8b98ca9459036
西村祐輝 <nishimurayuki@mullan-mac.local>
2020-01-13 20:07:29 +0900

    indexのデザイン

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,43 @@
+
+<section id="userPage" class="userPage">
+		<div class="section-area">
+			<p class="section-title">DEMO 3</p>
+			<div class="contents-wrap">
+				<div class="contents-box3">
+					<img src="" alt="">
+					<h2>flexboxのメリット</h2>
+					<p>flexboxは、要素の横並びなど配列する際に使用するCSS3コードです。これまで、inline-block やfloat などを利用してきましたが、他にもたくさんのコードを記述しないと横並びにならなかったのです。手間のかかるものでした。しかし、これからはflexbox があるから大丈夫！簡単に実装できますよ。</p>
+				</div>
+
+				<div class="contents-box3">
+					<img src="" alt="">
+					<h2>flexboxのメリット</h2>
+					<p>flexboxは、要素の横並びなど配列する際に使用するCSS3コードです。これからはflexbox があるから大丈夫！簡単に実装できますよ。</p>
+				</div>
+
+				<div class="contents-box3">
+					<img src="" alt="">
+					<h2>flexboxのメリット</h2>
+					<p>flexboxは、要素の横並びなど配列する際に使用するCSS3コードです。簡単に実装できますよ。</p>
+				</div>
+
+				<div class="contents-box3">
+					<img src="" alt="">
+					<h2>flexboxのメリット</h2>
+					<p>flexboxは、要素の横並びなど配列する際に使用するCSS3コードです。これからはflexbox があるから大丈夫！簡単に実装できますよ。</p>
+				</div>
+
+				<div class="contents-box3">
+					<img src="" alt="">
+					<h2>flexboxのメリット</h2>
+					<p>flexboxは、要素の横並びなど配列する際に使用するCSS3コードです。簡単に実装できますよ。</p>
+				</div>
+
+				<div class="contents-box3">
+					<img src="" alt="">
+					<h2>flexboxのメリット</h2>
+					<p>flexboxは、要素の横並びなど配列する際に使用するCSS3コードです。これまで、inline-block やfloat などを利用してきましたが、他にもたくさんのコードを記述しないと横並びにならなかったのです。手間のかかるものでした。しかし、これからはflexbox があるから大丈夫！簡単に実装できますよ。</p>
+				</div>
+			</div>
+		</div>
+	</section>
